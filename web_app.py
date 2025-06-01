c3cf343922007b07d99b8265aacd367c0f56b7f4
daniel alfarezi <danielalfarezi6@gmail.com>
2025-06-01 21:29:05 +0700

    first commit

---
+++ web_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from jikanpy import Jikan
+
+app = Flask(__name__)
+jikan = Jikan()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    judul = None
+    anime = None
+    review = None
+    if request.method == 'POST':
+        judul = request.form['judul']
+        hasil = jikan.search('anime', judul)
+        anime = hasil['data'][0] if hasil['data'] else None
+        if anime:
+            reviews = jikan.anime(anime['mal_id'], extension='reviews')
+            review_data = reviews.get('data', [])
+            if review_data:
+                review = review_data[0]
+    return render_template('index.html', judul=judul, anime=anime, review=review)
+
+if __name__ == '__main__':
+    app.run(debug=True)

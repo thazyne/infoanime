7b02ef8690dc92fb9a47dd4f327cb21dbbbeb95d
daniel alfarezi <danielalfarezi6@gmail.com>
2025-06-16 00:36:13 +0700

    cccc

--- web_app.py
+++ web_app.py
@@ -4,6 +4,7 @@
 from jikanpy import Jikan
 import time
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -136,4 +137,5 @@ def index():
     return render_template('index.html', error=error, result=result, title=title)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
